6434813cce2e1662f878c95dfaa7c98c296aab74
Suraj Prakash <16456012+suraj083@users.noreply.github.com>
2023-02-08 19:11:53 +0530

    corrected dict definition

--- sample_field.py
+++ sample_field.py
@@ -6,7 +6,7 @@
 # In this case we will only define a field class and the initialization that was happening inside the model class will happen outside
 # 
 sample_field_id_list = ['sm_higgs', 'bsm_scalar', 'w_boson_t', 'w_boson_l', 'z_boson_t', 'z_boson_l', 'photon_l' 't_quark', 'b_quark']
-sample_param_dict = ['g1':1, 'g2':1, 'yt':1, 'yb':1, 'mHsq':1, 'mSsq':1, 'lmbd':1, 'lmbd_SH':1, 'lmbd_S':1]
+sample_param_dict = {'g1':1, 'g2':1, 'yt':1, 'yb':1, 'mHsq':1, 'mSsq':1, 'lmbd':1, 'lmbd_SH':1, 'lmbd_S':1}
 #
 #
 #
