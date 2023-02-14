6c3c20a73aa2bf7e0702729dd17c1511779a9ead
Suraj Prakash <16456012+suraj083@users.noreply.github.com>
2023-02-14 22:06:29 +0530

    error handling in model initialization

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 """Created on Monday, February 6th 2023
    Author: Suraj Prakash
 """
-
+from field import field
 # Here we will create a model class whose subclasses would correspond to the various example cases - sm, bsm, sm+dilaton, bsm + dilaton,
 # 
 #
@@ -26,18 +26,45 @@
 #    b_params = {'yb': self.params['yb']}
 # -------------------------------------------------------------------   
 
+params_sm = ['g1', 'g2', 'yt', 'yb', 'mHsq', 'lmbd']
+params_bsm = ['g1', 'g2', 'yt', 'yb', 'mHsq', 'mSsq', 'lmbd', 'lmbd_SH', 'lmbd_S', 'N']
+params_eft = ['g1', 'g2', 'yt', 'yb', 'mHsq', 'lmbd'] # add Wilson Coefficients here
+
 class model:
-   default_field_list = ['sm_higgs', 'goldstone', 'bsm_scalar', 'w_boson_t', 'w_boson_l', 'z_boson_t', 'z_boson_l', 'photon_l', 't_quark', 'b_quark']
-   all_params = ['g1', 'g2', 'yt', 'yb', 'mHsq', 'mSsq', 'lmbd', 'lmbd_SH', 'lmbd_S', 'N']
+   _default_field_list = ['sm_higgs', 'goldstone', 'bsm_scalar', 'w_boson_t', 'w_boson_l', 'z_boson_t', 'z_boson_l', 'photon_l', 't_quark', 'b_quark']
 
-   def __init__(self, param_dict: dict, field_list: list = default_field_list, bsm: bool = True, eft: bool = False, large_T_approx: bool = False):
+   def __init__(self, param_dict: dict, field_list: list = _default_field_list, bsm: bool = True, eft: bool = False, large_T_approx: bool = False):
       
-      assert set(field_list).issubset(set(self.default_field_list)), "Invalid field name in field_list"
-      self.fields = field_list
+      try:
+         if (not eft):
+            if bsm:
+               assert set(param_dict.keys()).__eq__(set(params_bsm))
+            else:
+               assert set(param_dict.keys()).__eq__(set(params_sm))
 
-      assert set(param_dict.keys()).issubset(set(self.all_params))
+         else:
+            if bsm:
+               raise ValueError
+            else:
+               assert set(param_dict.keys()).__eq__(set(params_eft))
+
+      except ValueError:
+         print("Model cannot be initialized with both EFT and BSM parameters simultaneously set to true.")
+
+      except AssertionError:
+         print("Wrong or missing parameter.")
+
       self.params = param_dict
-      
+
+      assert set(field_list).issubset(set(self._default_field_list)), "Invalid field name in field_list"
+      self.field_name_list = field_list
+
       self.is_bsm = bsm
       self.is_eft = eft
       self.large_T_approx = large_T_approx
+
+      self.field_object_list = [field(field_name=name, param_dict=self.params, bsm=self.is_bsm, eft=self.is_eft) for name in self.field_name_list]
+ 
+   def __str__(self) -> str:
+      pass
+      
