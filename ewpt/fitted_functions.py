aeee29065fa281af709d4fc01ffb0d623864d0bf
Suraj Prakash <16456012+suraj083@users.noreply.github.com>
2023-03-08 19:08:01 +0530

    tested the updated thermal function

--- ewpt/fitted_functions.py
+++ ewpt/fitted_functions.py
@@ -11,29 +11,29 @@ model_Jb = keras.models.load_model(f"{os.getcwd()}/ewpt/Jb_model")
 model_Jf = keras.models.load_model(f"{os.getcwd()}/ewpt/Jf_model") 
 model_xlogx = keras.models.load_model(f"{os.getcwd()}/ewpt/xlogx_model")
 
-# def fitted_JB(var):
-#     return model_Jb(var)
+def fitted_JB(var):
+    return model_Jb(var)
 
-def fitted_JB(var): 
-    if tf.shape(var).shape[0] == 1:
-        return model_Jb(var)
-    elif tf.shape(var).shape[0] == 0:   # useful when creating single variable plots
-        new_var = tf.reshape(var, (1,))
-        return model_Jb(new_var)
-    elif tf.shape(var).shape[0] == 2:   # useful when creating meshgrids for contour or 3d plots
-        return tf.stack([model_Jb(elem) for elem in tf.unstack(var)])[:,:,0]
+# def fitted_JB(var): 
+#     if tf.shape(var).shape[0] == 1:
+#         return model_Jb(var)
+#     elif tf.shape(var).shape[0] == 0:   # useful when creating single variable plots
+#         new_var = tf.reshape(var, (1,))
+#         return model_Jb(new_var)
+#     elif tf.shape(var).shape[0] == 2:   # useful when creating meshgrids for contour or 3d plots
+#         return tf.stack([model_Jb(elem) for elem in tf.unstack(var)])[:,:,0]
 
-# def fitted_JF(var):
-#     return model_Jf(var)
+def fitted_JF(var):
+    return model_Jf(var)
 
-def fitted_JF(var): 
-    if tf.shape(var).shape[0] == 1:
-        return model_Jf(var)
-    elif tf.shape(var).shape[0] == 0:
-        new_var = tf.reshape(var, (1,))
-        return model_Jf(new_var)
-    elif tf.shape(var).shape[0] == 2:
-        return tf.stack([model_Jf(elem) for elem in tf.unstack(var)])[:,:,0]
+# def fitted_JF(var): 
+#     if tf.shape(var).shape[0] == 1:
+#         return model_Jf(var)
+#     elif tf.shape(var).shape[0] == 0:
+#         new_var = tf.reshape(var, (1,))
+#         return model_Jf(new_var)
+#     elif tf.shape(var).shape[0] == 2:
+#         return tf.stack([model_Jf(elem) for elem in tf.unstack(var)])[:,:,0]
 
 def fitted_dJB(var):
     var_b = tf.cast(var, tf.float32)
